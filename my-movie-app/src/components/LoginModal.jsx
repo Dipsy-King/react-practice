1d0597e135a4c0223a82f4e29e77fab09372466e
Petruska <pma.santos1993@gmail.com>
2025-02-25 16:34:15 +0000

    added flicp effect

--- my-movie-app/src/components/LoginModal.jsx
+++ my-movie-app/src/components/LoginModal.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function LoginModal({ ref }) {
+	const [isRotated, setIsRotated] = useState(false); // State to track the rotation
+
+	const handleFlip = () => {
+		setIsRotated(!isRotated); // Toggle the rotation state
+	};
+
 	return (
 		<>
 			<dialog ref={ref}>
-				<h1>Login</h1>
-				<form method="dialog">
+				<form className="modal-container" method="dialog">
+					<button type="button" onClick={handleFlip}>
+						Sign Up
+					</button>
+					<div className={`modal-content ${isRotated ? "flipped" : ""}`}>
+						<div className="front">frohnt</div>
+						<div className="back">back</div>
+					</div>
 					<button>Close</button>
 				</form>
 			</dialog>
